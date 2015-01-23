7f9aed6751339c819617614a031ce811db34da6a
Maxim Volgin <maxim.volgin@me.com>
2015-01-23 21:07:04 +0100

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+# locale
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+sudo locale-gen en_US.UTF-8
+
+# update
+sudo apt-get update
+
+# bonjour
+sudo apt-get install avahi-daemon
+
+
+
