fdd34599988a1d27ff1512dc29d14c7fdb0ceeb7
Maxim Volgin <maxim.volgin@me.com>
2015-01-30 13:32:49 +0100

    pn532nfc

---
+++ pn532nfc.sh
@@ -0,0 +1,6 @@
+wget ftp://imall.iteadstudio.com/Modules/IM130625002_ITEAD_PN532_NFC/library_Raspberry_NFC.zip
+wget ftp://imall.iteadstudio.com/Modules/IM130625002_ITEAD_PN532_NFC/Library_IM130625002_ITEAD_PN532_NFC.zip
+unzip library_Raspberry_NFC.zip
+unzip Library_IM130625002_ITEAD_PN532_NFC.zip
+rm library_Raspberry_NFC.zip
+rm Library_IM130625002_ITEAD_PN532_NFC.zip
